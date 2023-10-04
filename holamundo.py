d1be6c56a10a40265185582f284d39fcd3d23ab3
Andres Arango <arango850@gmail.com>
2023-10-04 10:47:30 -0500

    Gramatica agregada

--- holamundo.py
+++ holamundo.py
@@ -1,15 +1,1 @@
-# Pedir al usuario que ingrese tres números
-numero1 = float(input("Ingrese el primer número: "))
-numero2 = float(input("Ingrese el segundo número: "))
-numero3 = float(input("Ingrese el tercer número: "))
-
-# Comparar los números para encontrar el menor
-if numero1 <= numero2 and numero1 <= numero3:
-    menor = numero1
-elif numero2 <= numero1 and numero2 <= numero3:
-    menor = numero2
-else:
-    menor = numero3
-
-# Mostrar el resultado
-print("El menor número es:", menor)
+print("Hola Mundo")
